79b42773925916cb65ccb1cf434b5e6ac0573a48
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 13:27:30 +0400

    Change example status to out of maintenance

--- CS/CheckedComboBoxBoundToCollection/EntityCache.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-namespace CheckedComboBoxBoundToCollection {
-    public static class EntityCache {
-        public static Dictionary<int, Order> Orders = new Dictionary<int, Order>();
-    }
-}
--- VB/CheckedComboBoxBoundToCollection/EntityCache.vb
+++
@@ -1,8 +0,0 @@
-Imports Microsoft.VisualBasic
-Imports System.Collections.Generic
-
-Namespace CheckedComboBoxBoundToCollection
-	Public NotInheritable Class EntityCache
-		Public Shared Orders As New Dictionary(Of Integer, Order)()
-	End Class
-End Namespace
